dc193caab32dabbf4a109c640f353997529828cd
Mathues Fellipi <47674343+MatheusFellipi@users.noreply.github.com>
2021-03-14 18:08:46 -0300

    exercise08

---
+++ src/exercise08.js
@@ -0,0 +1,20 @@
+/* E8
+**Para o seguinte exercício** considere o array de objetos:
+[
+    {id: 1, first_name: ‘Juca’, last_name: ‘Da Silva’, age: 42},
+    {id: 2, first_name: ‘Daniel’, last_name: ‘Gonçalves’,  age: 21},
+    {id: 3, first_name: ‘Matheus’, last_name: ‘Garcia’, age: 28},
+    {id: 4, first_name: ‘Gabriel’, last_name: ‘Dorneles’,  age: 21}
+]
+
+Imprima todos os elementos em que a idade é menor que 30.
+
+ */
+
+
+window.exercise08 = function() {
+  console.log("resultado ex8: ");
+  buscarIdade(30);
+};
+
+// usei as mesma funcao do execise 07
